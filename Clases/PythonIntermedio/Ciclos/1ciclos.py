850daae15821db4676e4d2cf66af712a27644351
Irvin Soriano <irving.soriano31@gmail.com>
2025-09-27 04:56:31 -0600

    datacamp

---
+++ Clases/PythonIntermedio/Ciclos/1ciclos.py
@@ -0,0 +1,25 @@
+#While
+'''
+Realmente nada muy nuevo
+'''
+#for 
+'''
+Realmente nada muy nuevo
+'''
+print("WHILE\n")
+error=50.0
+while error>1:
+    error/=4
+    print(error)
+print("fin del ciclo")
+
+
+##Rercorde un poquito como se enumeraban
+print("\nFOR\n")
+
+fam=[1.73,1.68,1.71,1.89]
+
+for index, height in enumerate(fam):
+    print(f"{index} - {height}")
+
+
